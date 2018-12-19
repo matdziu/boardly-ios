e4e54cf3b92dd393e0f49072b655bf3c807c7ba7
Mateusz Dziubek <matdziu@gmail.com>
2018-12-19 20:27:38 +0100

    Added necessary views to storyboard

---
+++ Boardly/Login/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  Boardly
+//
+//  Created by Mateusz Dziubek on 19/12/2018.
+//  Copyright © 2018 Mateusz Dziubek. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewController: BaseNavViewController {
+    
+}
