ddc250c8aaa54c89f5dadc2a5c45084c5c53e66e
Mateusz Dziubek <matdziu@gmail.com>
2018-12-25 11:47:14 +0100

    Added models

---
+++ Boardly/Edit Profile/models/EditInputProfileData.swift
@@ -0,0 +1,13 @@
+//
+//  InputData.swift
+//  Boardly
+//
+//  Created by Mateusz Dziubek on 25/12/2018.
+//  Copyright © 2018 Mateusz Dziubek. All rights reserved.
+//
+import Foundation
+
+struct EditProfileInputData {
+    var name: String
+    var profilePictureUrl: URL?
+}
---
+++ Boardly/Edit Profile/models/ProfileData.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileData.swift
+//  Boardly
+//
+//  Created by Mateusz Dziubek on 25/12/2018.
+//  Copyright © 2018 Mateusz Dziubek. All rights reserved.
+//
+
+struct ProfileData {
+    var name: String = ""
+    var profilePicture: String = ""
+    var rating: Double? = nil
+}
