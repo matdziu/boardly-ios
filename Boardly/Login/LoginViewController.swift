bac0e93d078a3c3e9a00459f34325e19b86c2671
Mateusz Dziubek <matdziu@gmail.com>
2018-12-27 14:20:15 +0100

    Revert "TO REVERT"
    This reverts commit 321d0f660938a45693dddc91502c799d25d94305.

--- Boardly/Login/LoginViewController.swift
+++ Boardly/Login/LoginViewController.swift
@@ -107,8 +107,8 @@ class LoginViewController: BaseNavViewController, LoginView, GIDSignInUIDelegate
         showLoginError(error: loginViewState.error, errorMessage: loginViewState.errorMessage, dismissError: loginViewState.dismissError)
         
         if loginViewState.loginSuccess && loginViewState.isProfileFilled {
-            if let editProfileViewController = storyboard?.instantiateViewController(withIdentifier: EDIT_PROFILE_VIEW_CONTROLLER_ID) as? EditProfileViewController {
-                navigationController?.setViewControllers([editProfileViewController], animated: true)
+            if let homeViewController = storyboard?.instantiateViewController(withIdentifier: HOME_VIEW_CONTROLLER_ID) as? HomeViewController {
+                navigationController?.setViewControllers([homeViewController], animated: true)
             }
         } else if loginViewState.loginSuccess && !loginViewState.isProfileFilled {
             if let editProfileViewController = storyboard?.instantiateViewController(withIdentifier: EDIT_PROFILE_VIEW_CONTROLLER_ID) as? EditProfileViewController {
