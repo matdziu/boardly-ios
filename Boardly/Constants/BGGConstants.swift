021336f65270992edba77a33c52535ad94d1d7a6
Mateusz Dziubek <matdziu@gmail.com>
2019-01-09 07:08:38 +0100

    Added BGGConstants file

---
+++ Boardly/Constants/BGGConstants.swift
@@ -0,0 +1,12 @@
+//
+//  BGGConstants.swift
+//  Boardly
+//
+//  Created by Mateusz Dziubek on 09/01/2019.
+//  Copyright © 2019 Mateusz Dziubek. All rights reserved.
+//
+
+let BGG_URL = "https://www.boardgamegeek.com/xmlapi2"
+let SEARCH_URL = "\(BGG_URL)/search?type=boardgame,rpg"
+let BOARD_GAME_DETAILS_URL = "\(BGG_URL)/thing&type=boardgame"
+let RPG_DETAILS_URL = "\(BGG_URL)/family&type=rpg"
