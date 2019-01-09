480610513c7bcf92ff2f45de6c4f399282005ff7
Mateusz Dziubek <matdziu@gmail.com>
2019-01-09 15:26:32 +0100

    Added basic implementation of methods in PickGameViewController

--- Boardly/Pick Game/PickGameViewController.swift
+++ Boardly/Pick Game/PickGameViewController.swift
@@ -8,7 +8,34 @@
 
 import Foundation
 import UIKit
+import RxSwift
 
-class PickGameViewController: BaseNavViewController {
+class PickGameViewController: BaseNavViewController, PickGameView {
     
+    private var querySubject: PublishSubject<String>!
+    
+    private let pickGamePresenter = PickGamePresenter(pickGameInteractor: PickGameInteractorImpl(gameService: GameServiceImpl()))
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        initEmitters()
+        pickGamePresenter.bind(pickGameView: self)
+    }
+    
+    private func initEmitters() {
+        querySubject = PublishSubject()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        pickGamePresenter.unbind()
+        super.viewWillDisappear(animated)
+    }
+    
+    func render(pickGameViewState: PickGameViewState) {
+        
+    }
+    
+    func queryEmitter() -> Observable<String> {
+        return querySubject
+    }
 }
