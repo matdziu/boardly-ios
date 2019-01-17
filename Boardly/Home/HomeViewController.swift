2395212555daf8b13aea4789f8328b25d2e10cb7
Mateusz Dziubek <matdziu@gmail.com>
2019-01-17 08:17:11 +0100

    Setup delagates

--- Boardly/Home/HomeViewController.swift
+++ Boardly/Home/HomeViewController.swift
@@ -11,4 +11,38 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var eventsCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        eventsCollectionView.dataSource = self
+        eventsCollectionView.delegate = self
+    }
+    
+    private var events: [BoardlyEvent] = [] {
+        didSet {
+            eventsCollectionView.reloadData()
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let boardlyEventCell = collectionView.dequeueReusableCell(withReuseIdentifier: EVENT_CELL_ID, for: indexPath) as! BoardlyEventCell
+        let event = events[indexPath.row]
+        return boardlyEventCell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 334)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
 }
