db12882fe8f856daf6eca7ec45b59505d7f97a90
Mateusz Dziubek <matdziu@gmail.com>
2019-01-24 07:55:06 +0100

    Added created, accepted and pending labels

--- Boardly/Home/BoardlyEventCell.swift
+++ Boardly/Home/BoardlyEventCell.swift
@@ -24,11 +24,39 @@ class BoardlyEventCell: UITableViewCell {
     @IBOutlet weak var descriptionButton: UIButton!
     @IBOutlet weak var timeImageView: UIImageView!
     @IBOutlet weak var placeImageView: UIImageView!
+    @IBOutlet weak var createdLabel: UILabel!
+    @IBOutlet weak var acceptedLabel: UILabel!
+    @IBOutlet weak var pendingLabel: UILabel!
     
     private let renderer = EventUIRenderer()
     
     func bind(event: BoardlyEvent) {
         selectionStyle = .none
         renderer.displayEventInfo(event: event, eventNameLabel: nameLabel, gameLabel: game1NameLabel, placeButton: placeButton, locationImageView: placeImageView, boardGameImageView: game1ImageView, seeDescriptionButton: descriptionButton, dateButton: dateButton, timeImageView: timeImageView, gameLabel2: game2NameLabel, boardGameImageView2: game2ImageView, gameLabel3: game3NameLabel, boardGameImageView3: game3ImageView)
+        setTypeLabel(type: event.type)
+    }
+    
+    private func setTypeLabel(type: EventType) {
+        switch type {
+        case .CREATED:
+            makeVisibleOnly(selectedView: createdLabel)
+        case .ACCEPTED:
+            makeVisibleOnly(selectedView: acceptedLabel)
+        case .PENDING:
+            makeVisibleOnly(selectedView: pendingLabel)
+        case .DEFAULT:
+            makeVisibleOnly(selectedView: joinButton)
+        }
+    }
+    
+    private func makeVisibleOnly(selectedView: UIView) {
+        let viewsList = [joinButton, createdLabel, acceptedLabel, pendingLabel]
+        for view in viewsList {
+            if selectedView == view {
+                view?.isHidden = false
+            } else {
+                view?.isHidden = true
+            }
+        }
     }
 }
