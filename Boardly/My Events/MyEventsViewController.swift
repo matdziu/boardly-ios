342f73453b89708ba00488d2ccd50c8dc0c81502
Mateusz Dziubek <matdziu@gmail.com>
2019-01-28 18:22:29 +0100

    Added MyEventsViewController implementation

--- Boardly/My Events/MyEventsViewController.swift
+++ Boardly/My Events/MyEventsViewController.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import UIKit
+import RxSwift
+
+class MyEventsViewController: UIViewController, MyEventsView {
+    
+    private var fetchEventsTriggerSubject = PublishSubject<Bool>()
+    private var joinEventSubject = PublishSubject<JoinEventData>()
+    private let myEventsPresenter = MyEventsPresenter(myEventsInteractor: MyEventsInteractorImpl(myEventsService: MyEventsServiceImpl()))
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        initEmitters()
+        myEventsPresenter.bind(myEventsView: self)
+    }
+    
+    private func initEmitters() {
+        fetchEventsTriggerSubject = PublishSubject()
+        joinEventSubject = PublishSubject()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        myEventsPresenter.unbind()
+        super.viewWillDisappear(animated)
+    }
+    
+    func fetchEventsTriggerEmitter() -> Observable<Bool> {
+        return fetchEventsTriggerSubject
+    }
+    
+    func joinEventEmitter() -> Observable<JoinEventData> {
+        return joinEventSubject
+    }
+    
+    func render(myEventsViewState: MyEventsViewState) {
+        
+    }
+}
