e1a6f3286d1002889e31459e6a5b948eca0f0c93
Mateusz Dziubek <matdziu@gmail.com>
2019-01-28 21:25:44 +0100

    Took different approach to segmented control

--- Boardly/My Events/MyEventsViewController.swift
+++ Boardly/My Events/MyEventsViewController.swift
@@ -12,29 +12,42 @@ import RxSwift
 
 class MyEventsViewController: UIViewController, MyEventsView {
     
-    @IBOutlet weak var acceptedContainer: UIView!
-    @IBOutlet weak var createdContainer: UIView!
-    @IBOutlet weak var pendingContainer: UIView!
-    @IBOutlet weak var interestingContainer: UIView!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
-    
-    private var containerViews: [UIView] = []
+    @IBOutlet weak var eventsTableView: UITableView!
+    @IBOutlet weak var progressView: UIActivityIndicatorView!
     
     private var fetchEventsTriggerSubject = PublishSubject<Bool>()
     private var joinEventSubject = PublishSubject<JoinEventData>()
     private let myEventsPresenter = MyEventsPresenter(myEventsInteractor: MyEventsInteractorImpl(myEventsService: MyEventsServiceImpl()))
+    private var initialize = true
+    
+    private var renderedEvents: [BoardlyEvent] = [] {
+        didSet {
+            eventsTableView.reloadData()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                self.eventsTableView.scrollToTop()
+            }
+        }
+    }
+    
+    private var acceptedEvents: [BoardlyEvent] = []
+    private var pendingEvents: [BoardlyEvent] = []
+    private var createdEvents: [BoardlyEvent] = []
+    private var interestingEvents: [BoardlyEvent] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        containerViews = [acceptedContainer, createdContainer, pendingContainer, interestingContainer]
         segmentedControl.selectedSegmentIndex = 0
-        showOnly(acceptedContainer)
+        eventsTableView.dataSource = self
+        eventsTableView.delegate = self
+        eventsTableView.tableFooterView = UIView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         initEmitters()
         myEventsPresenter.bind(myEventsView: self)
+        fetchEventsTriggerSubject.onNext(initialize)
     }
     
     private func initEmitters() {
@@ -44,6 +57,7 @@ class MyEventsViewController: UIViewController, MyEventsView {
     
     override func viewWillDisappear(_ animated: Bool) {
         myEventsPresenter.unbind()
+        initialize = false
         super.viewWillDisappear(animated)
     }
     
@@ -58,29 +72,78 @@ class MyEventsViewController: UIViewController, MyEventsView {
     @IBAction func containerSwitched(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
         case 0:
-            showOnly(acceptedContainer)
+            renderedEvents = acceptedEvents
         case 1:
-            showOnly(createdContainer)
+            renderedEvents = createdEvents
         case 2:
-            showOnly(pendingContainer)
+            renderedEvents = pendingEvents
         case 3:
-            showOnly(interestingContainer)
+            renderedEvents = interestingEvents
         default:
-            showOnly(acceptedContainer)
-        }
-    }
-    
-    private func showOnly(_ container: UIView) {
-        for item in containerViews {
-            if item == container {
-                item.alpha = 1
-            } else {
-                item.alpha = 0
-            }
+            renderedEvents = []
         }
     }
     
     func render(myEventsViewState: MyEventsViewState) {
+        showProgress(show: myEventsViewState.progress)
+        acceptedEvents = myEventsViewState.acceptedEvents
+        pendingEvents = myEventsViewState.pendingEvents
+        createdEvents = myEventsViewState.createdEvents
+        interestingEvents = myEventsViewState.interestingEvents
+        containerSwitched(segmentedControl)
+    }
+    
+    private func showProgress(show: Bool) {
+        if show {
+            progressView.startAnimating()
+        } else {
+            progressView.stopAnimating()
+        }
+    }
+    
+    private func enterEvent(event: BoardlyEvent) {
         
+    }
+    
+    private func joinEvent(eventId: String) {
+        
+    }
+}
+
+extension MyEventsViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return renderedEvents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let boardlyEventCell = eventsTableView.dequeueReusableCell(withIdentifier: EVENT_CELL_ID, for: indexPath) as! BoardlyEventCell
+        let event = renderedEvents[indexPath.row]
+        boardlyEventCell.bind(event: event, cellClickAction: { self.cellClickAction(event: event) })
+        return boardlyEventCell
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 350
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let event = renderedEvents[indexPath.row]
+        cellClickAction(event: event)
+    }
+    
+    private func cellClickAction(event: BoardlyEvent) {
+        switch event.type {
+        case .CREATED, .ACCEPTED:
+            enterEvent(event: event)
+        case .PENDING:
+            return
+        case .DEFAULT:
+            joinEvent(eventId: event.eventId)
+        }
     }
 }
