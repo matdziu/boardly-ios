94d9d872eea6948cb529b10ac6e74f441af5fee1
Mateusz Dziubek <matdziu@gmail.com>
2019-02-05 13:29:22 +0100

    Added player ui renderer empty file

---
+++ Boardly/Common/PlayerUIRenderer.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerUIRenderer.swift
+//  Boardly
+//
+//  Created by Mateusz Dziubek on 05/02/2019.
+//  Copyright © 2019 Mateusz Dziubek. All rights reserved.
+//
+
+import Foundation
+
+class PlayerUIRenderer {
+    
+}
