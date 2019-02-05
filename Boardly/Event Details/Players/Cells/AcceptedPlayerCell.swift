3c5341285b795f675058dde01055db931ec9a7aa
Mateusz Dziubek <matdziu@gmail.com>
2019-02-05 13:15:23 +0100

    Added cell files

---
+++ Boardly/Event Details/Players/Cells/AcceptedPlayerCell.swift
@@ -0,0 +1,19 @@
+//
+//  AcceptedPlayerCell.swift
+//  Boardly
+//
+//  Created by Mateusz Dziubek on 05/02/2019.
+//  Copyright © 2019 Mateusz Dziubek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AcceptedPlayerCell: UITableViewCell {
+    
+    @IBOutlet weak var playerNameLabel: UILabel!
+    @IBOutlet weak var playerPicture: UIImageView!
+    @IBOutlet weak var playerRatingLabel: UILabel!
+    @IBOutlet weak var rateButton: UIButton!
+    @IBOutlet weak var helloTextLabel: UILabel!
+}
---
+++ Boardly/Event Details/Players/Cells/PlayerControlCell.swift
@@ -0,0 +1,16 @@
+//
+//  PlayerControlCell.swift
+//  Boardly
+//
+//  Created by Mateusz Dziubek on 05/02/2019.
+//  Copyright © 2019 Mateusz Dziubek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlayerControlCell: UITableViewCell {
+    
+    @IBOutlet weak var chatNotificationsSwitch: UISwitch!
+    @IBOutlet weak var leaveEventButton: UIButton!
+}
