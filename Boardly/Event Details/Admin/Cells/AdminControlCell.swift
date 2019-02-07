48b25f8661507c05308b939895b1fc87ffac3755
Mateusz Dziubek <matdziu@gmail.com>
2019-02-07 08:45:45 +0100

    Added behavior to admin control cell

--- Boardly/Event Details/Admin/Cells/AdminControlCell.swift
+++ Boardly/Event Details/Admin/Cells/AdminControlCell.swift
@@ -13,6 +13,16 @@ class AdminControlCell: UITableViewCell {
     
     @IBOutlet weak var editEventButton: UIButton!
     
+    func bind(editEventHandler: @escaping () -> ()) {
+        editEventButton.userInfo[EDIT_EVENT_HANDLER_USER_INFO] = editEventHandler
+        editEventButton.addTarget(self, action: #selector(editEventButtonClicked(_:)), for: .touchUpInside)
+    }
+    
+    @objc private func editEventButtonClicked(_ sender: UIButton) {
+        let editEventButtonHandler = sender.userInfo[EDIT_EVENT_HANDLER_USER_INFO] as? () -> () ?? {}
+        editEventButtonHandler()
+    }
+    
     @IBAction func switchChanged(_ sender: UISwitch) {
         
     }
